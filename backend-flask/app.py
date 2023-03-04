3f8a96a360f9b3acd5d6bb53f2778f7fd4c9b270
tembo84 <slimteezy@yahoo.com>
2023-03-04 19:56:37 +0000

    small edits

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -33,7 +33,7 @@ import logging
 from time import strftime
 
 #Rollbar
-import os
+#import os
 import rollbar
 import rollbar.contrib.flask
 from flask import got_request_exception
@@ -58,8 +58,8 @@ provider.add_span_processor(processor)
 #xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 # Show this in the logs with the backend-flask app (STDOUT)
-simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(simple_processor)
+# simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+# provider.add_span_processor(simple_processor)
 
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
