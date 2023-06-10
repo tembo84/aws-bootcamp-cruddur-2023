2ccdd3f0c68cd58289c73fa9c7dd5526d2373d54
tembo84 <slimteezy@yahoo.com>
2023-06-10 23:38:44 +0000

    fix migration

--- backend-flask/db/migrations/1686433279446285_reply_to_activity_uuid_to_string.py
+++ backend-flask/db/migrations/1686433279446285_reply_to_activity_uuid_to_string.py
@@ -9,8 +9,8 @@ class ReplyToActivityUuidToStringMigration:
     return data
   def rollback_sql():
     data = """
-    ALTER TABLE activities
-    ALTER COLUMN reply_to_activity_uuid TYPE integer USING (reply_to_activity_uuid::integer);
+    ALTER TABLE activities DROP COLUMN reply_to_activity_uuid;
+    ALTER TABLE activities ADD COLUMN reply_to_activity_uuid integer;
     """
     return data
 
